30c9b6c5d34069f4f125dd8aff6219ea00c63e35
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-05-12 23:49:17 -0400

    initial commit and running server port 5000

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const postsRouter = require('./posts/postRouter')
 
 const server = express();
+
+server.use(express.json())
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('/api/posts', postsRouter)
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method
+  const endpoint = req.originalUrl
+
+  console.log(`${method} to ${endpoint}`)
+
+  next()
+}
+
+server.use(logger)
 
 module.exports = server;
