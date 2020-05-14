d02822d8da67e431ed0b118312b2e2cfd0dc387f
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-05-13 21:12:46 -0400

    added the postsRouter and usersRouter

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const helmet = require('helmet')
+
+const userRouter = require('./users/userRouter')
 const postsRouter = require('./posts/postRouter')
 
 const server = express();
 
 server.use(express.json())
+server.use(helmet())
+server.use(logger)
+
+server.use('/api/posts', postsRouter)
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use('/api/posts', postsRouter)
 
 //custom middleware
 
@@ -17,12 +24,8 @@ function logger(req, res, next) {
   const method = req.method
   const endpoint = req.url
   const timestamp = Date.now()
-
   console.log(`${method} to ${endpoint} at ${timestamp}`)
-
   next()
 }
-
-server.use(logger)
 
 module.exports = server;
